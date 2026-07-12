a7f5f60486045d9497eb10fe7328168c2d6911e3
Abdullah Azher Chaudhary <117835393+ABDULLAHAZHERCH@users.noreply.github.com>
2026-07-13 01:11:47 +0500

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        order = {")": "(", "}": "{", "]": "["}
+        stack = []
+
+        for c in s:
+            if c in order:
+                if len(stack) == 0 or stack[-1] != order[c]:
+                    return False
+                else:
+                    stack.pop()
+            else:
+                stack.append(c)
+        return len(stack) == 0
